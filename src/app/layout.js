363dc6bfe57bcfbd6dfc9358ffa39313783670b0
Hiraina-RND <hei.hiraina@gmail.com>
2026-05-12 14:11:44 +0300

    init Next.js project with Tailwind

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"], // font-light, font-normal, font-medium, font-semibold, font-bold
+  variable: "--font-poppins",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${poppins.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
